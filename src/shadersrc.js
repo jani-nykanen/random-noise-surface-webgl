3ec7666937b15ccd68868a89e4e03e471b580a12
Jani Nykänen <jnyknn@gmail.com>
2020-07-17 14:45:20 +0300

    Changed some shader sources, not tested yet

--- src/shadersrc.js
+++ src/shadersrc.js
@@ -2,8 +2,7 @@
 
 const FRAG_FUNCTIONS = 
 `
-vec3 rgb332(vec3 v) {
-
+vec3 rgb222(vec3 v) {
     float div = 85.0;
     vec3 ret;
     
@@ -19,41 +18,39 @@ vec3 rgb332(vec3 v) {
     ret.g = floor(ret.g / 2.0) / 3.0;
     ret.b = floor(ret.b / 2.0) / 3.0;
     
-/*
-    ret.r = (floor(v.r * 255.0 / divRG) * divRG) / 255.0;
-    ret.g = (floor(v.g * 255.0 / divRG) * divRG) / 255.0;
-    ret.b = (floor(v.b * 255.0 / divB) * divB) / 255.0;
-*/
     return ret;
 }`
 
 
 const VERTEX_SOURCE = {
 
-texturedLight : 
+noTextureLight : 
 
 `attribute vec3 vertexPos;
 attribute vec2 vertexUV;
 attribute vec3 vertexNormal;
+attribute vec3 vertexColor;
    
 uniform mat4 transform;
 uniform mat4 rotation;
 uniform vec3 pos;
 uniform vec3 size;
 uniform vec3 lightDir;
+uniform vec4 color;
 uniform float lightMag;
-varying vec2 uv;
+
 varying float light;
+varying vec4 faceColor;
    
 void main() {
     vec3 p = vertexPos * size + pos;
     vec4 o = transform * vec4(p, 1);
     gl_Position = o;
     
-    uv = vertexUV;
-    
     vec3 rot = (rotation * vec4(vertexNormal,1)).xyz;
     light = (1.0-lightMag) + lightMag * dot(rot, lightDir);
+	
+	faceColor = color * vec4(vertexColor, 1.0);
 }`,
 
 
@@ -62,6 +59,7 @@ texturedNoLight :
 `attribute vec3 vertexPos;
 attribute vec2 vertexUV;
 attribute vec3 vertexNormal;
+attribute vec3 vertexColor;
    
 uniform mat4 transform;
 uniform mat4 rotation;
@@ -69,7 +67,10 @@ uniform vec3 pos;
 uniform vec3 size;
 uniform vec3 lightDir;
 uniform float lightMag;
+uniform vec4 color;
+
 varying vec2 uv;
+varying vec4 faceColor;
    
 void main() {
     vec3 p = vertexPos * size + pos;
@@ -77,6 +78,7 @@ void main() {
     gl_Position = o;
     
     uv = vertexUV;
+	faceColor = color * vec4(vertexColor, 1.0);
 }`,
 
 
@@ -85,6 +87,7 @@ noTexture :
 `attribute vec3 vertexPos;
 attribute vec2 vertexUV;
 attribute vec3 vertexNormal;
+attribute vec3 vertexColor;
    
 uniform mat4 transform;
 uniform vec3 pos;
@@ -94,110 +97,84 @@ void main() {
     vec3 p = vertexPos * size + pos;
     vec4 o = transform * vec4(p, 1);
     gl_Position = o;
+	
+	faceColor = color * vec4(vertexColor, 1.0);
 }`
 };
 
 
 const FRAGMENT_SOURCE = {
 
-fogAndLightTextured :
+fogAndLightNoTexture :
 
 `precision mediump float;
  
 uniform sampler2D t0;
-uniform vec4 color;
 uniform vec4 fogColor;
 uniform float fogDensity;
-uniform vec2 texPos;
-uniform vec2 texSize;
-varying vec2 uv;
+
 varying float light;
+varying vec4 faceColor;
 `
 + FRAG_FUNCTIONS +
 `
 void main() {
-    const float DELTA = 0.001;
-    vec2 tex = uv;    
-    tex.x *= texSize.x;
-    tex.y *= texSize.y;
-    tex += texPos;
-    vec4 res = color * texture2D(t0, tex);
-    if(res.a <= DELTA) {
-        discard;
-    }
+
     vec4 a = gl_FragCoord;
     float z = a.z / a.w;
     float d = z * fogDensity;
     float fog = 1.0 / exp(d*d);
     fog = clamp(fog, 0.0, 1.0);
     gl_FragColor = vec4(
-        rgb332((1.0-light)*(fog*res.xyz + (1.0-fog)*fogColor.xyz)), 
-        res.a);
+        rgb222((1.0-light)*(fog*faceColor.xyz + (1.0-fog)*fogColor.xyz)), 
+        faceColor.a);
+		
 }`,
 
 
-lightTextured :
+lightNoTexture :
 
 `precision mediump float;
  
 uniform sampler2D t0;
-uniform vec4 color;
 uniform vec4 fogColor;
 uniform float fogDensity;
-uniform vec2 texPos;
-uniform vec2 texSize;
-varying vec2 uv;
+
 varying float light;
+varying vec4 faceColor;
 `
 + FRAG_FUNCTIONS +
 `
 void main() {
-    const float DELTA = 0.001;
-    vec2 tex = uv;    
-    tex.x *= texSize.x;
-    tex.y *= texSize.y;
-    tex += texPos;
-    vec4 res = color * texture2D(t0, tex);
-    if(res.a <= DELTA) {
-        discard;
-    }
-    gl_FragColor = vec4(rgb332((1.0-light)*res.xyz), res.a);
+
+    gl_FragColor = vec4(rgb222((1.0-light)*faceColor.xyz), faceColor.a);
 }`,
 
 
 
-fogTextured :
+fogNoTexture :
 
 `precision mediump float;
  
 uniform sampler2D t0;
-uniform vec4 color;
 uniform vec4 fogColor;
 uniform float fogDensity;
-uniform vec2 texPos;
-uniform vec2 texSize;
-varying vec2 uv;
+
+varying vec4 faceColor;
+
 `
 + FRAG_FUNCTIONS +
 `
 void main() {
-    const float DELTA = 0.001;
-    vec2 tex = uv;    
-    tex.x *= texSize.x;
-    tex.y *= texSize.y;
-    tex += texPos;
-    vec4 res = color * texture2D(t0, tex);
-    if(res.a <= DELTA) {
-        discard;
-    }
+
     vec4 a = gl_FragCoord;
     float z = a.z / a.w;
     float d = z * fogDensity;
     float fog = 1.0 / exp(d*d);
     fog = clamp(fog, 0.0, 1.0);
     gl_FragColor = vec4(
-        rgb332(fog*res.xyz + (1.0-fog)*fogColor.xyz), 
-        res.a);
+        rgb222(fog*faceColor.xyz + (1.0-fog)*fogColor.xyz), 
+        faceColor.a);
 }`,
 
 
@@ -206,11 +183,12 @@ textured :
 `precision mediump float;
  
 uniform sampler2D t0;
-uniform vec4 color;
 uniform vec2 texPos;
 uniform vec2 texSize;
+
 varying vec2 uv;
 varying float light;
+varying vec4 faceColor;
 `
 + FRAG_FUNCTIONS +
 `
@@ -220,23 +198,24 @@ void main() {
     tex.x *= texSize.x;
     tex.y *= texSize.y;
     tex += texPos;
-    vec4 res = texture2D(t0, tex) * color;
+    vec4 res = texture2D(t0, tex) * faceColor;
     if(res.a <= DELTA) {
         discard;
     }
-    gl_FragColor = vec4(rgb332(res.xyz), res.a);
+    gl_FragColor = vec4(rgb222(res.xyz), res.a);
 }`,
 
 
 noTexture :
 
 `precision mediump float;
-uniform vec4 color;
+
+varying vec4 faceColor;
 `
 + FRAG_FUNCTIONS +
 `
 void main() {
-    gl_FragColor = vec4(rgb332(color.rgb), color.a);
+    gl_FragColor = vec4(rgb222(faceColor.rgb), faceColor.a);
 }`
 
 };
